a6b3915644af6a4904821d7aef34681c5a2f136a
Charles J. Cliffe <cj@cubicproductions.com>
2016-07-19 17:58:39 -0400

    Unused function

--- scripts/getCubicSDRVer.sh
+++ scripts/getCubicSDRVer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function SET {
-SET $1=$2
-}
-
 eval $(cat build_stage/CubicSDR/CMakeLists.txt | grep \(CUBICSDR_VERSION | sed "s/SET[(]//" | sed "s/ \"/=\"/" | sed "s/[)]/; /")
 
 echo $CUBICSDR_VERSION
